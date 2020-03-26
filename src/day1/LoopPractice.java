05533b6d3d977727b9061784a0a42e61b3a725c3
55lala <lalaimanovaus@gmail.com>
2020-03-26 15:14:35 -0400

    modified LoopPractice class

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -4,5 +4,7 @@ public class LoopPractice {
     public static void main(String[] args) {
         String str = "Studying";
         System.out.println(str);
+        String str1 ="Selenium";
+        System.out.println(str1);
     }
 }
